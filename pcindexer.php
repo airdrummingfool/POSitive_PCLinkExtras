a20406f39f9aa1d2bf74a72e41c441bfabaaa5ce
Tommy Goode <tommy@irdequipment.com>
2015-06-08 18:20:12 -0500

    Adding all extra files necessary to PCLink product uploads, and a composer.json for installation via composer.

---
+++ pcindexer.php
@@ -0,0 +1,52 @@
+﻿<?php
+
+/*
+ * This file is used by POSitive Commerce PCLink during the upload process
+ *  to trigger a re-index of all indexes.
+ */
+
+ini_set("display_errors","On");
+set_time_limit(0);
+ini_set('memory_limit','1024M');
+require_once './app/Mage.php';
+umask( 0 );
+
+/*****Use for single Magento store*/
+//Mage :: app( "default" );
+/*****Use for multiple Magento stores - Might also work for single stores*/
+Mage :: app(Mage::app()->getStore()); 
+
+
+//Flush Catalog Images Cache
+Mage::getModel('catalog/product_image')->clearCache();
+//End flush
+
+//Clean Magento Cache
+Mage::app()->cleanCache();
+//End Clean Cache
+
+//John probably found this code from here - but I have modified it below
+//magento-zend.blogspot.com/2011/11/magento-indexing-through-command-line.html
+
+if(defined('STDIN') )//if it is via command line then
+{
+	echo("Running from CLI \n");
+	$processNo=isset($argv[1])?$argv[1] : 1;
+}
+else
+// otherwise do non command line stuff
+{
+	echo("Running through Browser <br>");
+	$processNo=(isset($_GET['process']))? $_GET['process'] : 1 ;
+}
+ 
+echo "\n<br>Started Indexing At:  " . date("d/m/y h:i:s");
+	for ($index = 1; $index <= 8; $index++) { //added this line
+	//$process = Mage::getModel('index/process')->load($processNo);
+	$process = Mage::getModel('index/process')->load($index); //changed $processNo to $index
+	$process->reindexAll();
+	}
+echo "\n<br>Indexing Completes At: ".date("d/m/y h:i:s");
+
+
+?>
---
+++ sku2sku.php
@@ -0,0 +1,36 @@
+<?php 
+
+/*
+ * This file is possibly used by PCLink to change the SKU of a product(?)
+ */
+
+include_once './app/Mage.php';
+Mage::app();
+ 
+$updates_file="./var/import/sku2sku.csv";
+ 
+$sku_entry=array();
+ 
+$updates_handle=fopen($updates_file, 'r');
+if($updates_handle) { 
+    while($sku_entry=fgetcsv($updates_handle, 1000, ",")) { 
+        $old_sku=$sku_entry[0];
+        $new_sku=$sku_entry[1];
+        echo "<br ?-->Updating ".$old_sku." to ".$new_sku." - ";
+        try {
+            $get_item = Mage::getModel('catalog/product')->loadByAttribute('sku', $old_sku);
+            if ($get_item) {
+                $get_item->setSku($new_sku)->save();
+                echo "successful";
+            } else {
+                echo "item not found";
+            }
+        } catch (Exception $e) { 
+            echo "Cannot retrieve products from Magento: ".$e->getMessage()."
+";
+            return;
+        }
+    }
+}
+ 
+fclose($updates_handle);
